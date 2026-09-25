ce1d46ee0206d67c68997c85415f130419bbd6ca
Kuansy <223074643+kuansy373@users.noreply.github.com>
2026-09-25 10:58:52 +0900

    fix: `APP_BASE`を修正。

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-export const APP_BASE = '/memo_app/client/';
+export const APP_BASE = '/memo_app/';
 export const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
   ? 'http://localhost:3000'
   : 'https://memo-app-server-bew5.onrender.com';
