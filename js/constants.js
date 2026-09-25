95998302d577755e8d0135d33d02d591375b1185
Kuansy <223074643+kuansy373@users.noreply.github.com>
2026-09-25 11:35:46 +0900

    fix: ローカルで開いたときに`//root`になる問題を修正。

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,7 @@
-export const APP_BASE = '/memo_app/';
+export const APP_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+  ? '/'
+  : '/memo_app/';
+
 export const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
   ? 'http://localhost:3000'
   : 'https://memo-app-server-bew5.onrender.com';
