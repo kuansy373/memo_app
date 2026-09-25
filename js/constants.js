c3aabcc4e518cd0b05bce707d5e0ecd6fce47421
Kuansy <223074643+kuansy373@users.noreply.github.com>
2026-09-26 06:10:55 +0900

    fix: ローカルでも開けるように修正、整理。

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,5 @@
-export const APP_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
-  ? '/'
-  : '/memo_app/';
+export const APP_BASE = '/memo_app/';
 
-export const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
-  ? 'http://localhost:3000'
+export const API_BASE = window.location.hostname === 'localhost'
+  ? 'http://localhost:5500'
   : 'https://memo-app-server-bew5.onrender.com';
--- server/server.js
+++ server/server.js
@@ -30,8 +30,8 @@ app.use(session({
   cookie: {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   },
 }));
 
